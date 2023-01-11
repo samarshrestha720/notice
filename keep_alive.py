1109a551be4eb37f94ced7324155746390adc706
Samar Shrestha <66566474+samarshrestha720@users.noreply.github.com>
2023-01-11 14:23:33 +0000

    Added DiscordBot

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from threading import Thread
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "Hello. I am alive!"
+  return render_template("index.html",title="NotifyNotice")
+
+@app.route('/success', methods=["POST"])
+def success():
+  phno=request.form.get("ph_no")
+  fName=request.form.get("fName")
+  CRN=request.form.get("CRN")
+  return render_template("success.html", title="Successful!", phno=phno, fName=fName, CRN=CRN)
+
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
+
 
 def keep_alive():
     t = Thread(target=run)
--- main.py
+++ main.py
@@ -1,79 +1,122 @@
 from bs4 import BeautifulSoup
-import requests, time
+import requests
 from keep_alive import keep_alive
 import pyshorteners
-import re
-import csv
+import re, csv
+import discord, os, asyncio,aiohttp
+from io import BytesIO
 
 type_bitly = pyshorteners.Shortener(
     api_key='daa876e13a562bea8962a76e506a74bc3d4cb746')  #bitly api key
+token = 'EAAMDySMjW2MBAHscR2ifFDdeHLy6Vn1dNa53NHxL5zrcnVhcsyF5qX42ZBYWMAqHGzrhpSPHyeS6YlmflRYaJyZCL5aY4U2IYz1887MuqXwh4qJyKsyeWfFSm7fEd4cg8L1CkQaOsaZBb8a3nOwdEIjxROmnc9Jd9TgAOKZAM9wFVJYEitWgex7wtTPd3RAZD'
+
+client = discord.Client(intents=discord.Intents.default())
+
+#thread = threading.Thread(target=disRun)
 
 
-def wtofile():  #write to file function
+def wtofile(title, url, shortUrl):  #write to file function
     with open('notices.csv', mode='a') as wfile:
-        rows = [title, url, shortUrl[1]]
+        rows = [title, url, shortUrl]
         writer = csv.writer(wfile)
         writer.writerow(rows)
         wfile.close()
 
 
-def sendSms():  #send sms function
-    msg = f"NEC New Notice:\n{title}\n{shortUrl[1]}\n-NotifyNotice"
+def sendSms(title, shortUrl):  #send sms function
+    msg = f"NEC New Notice:\n{title}\n{shortUrl}\n-NotifyNotice"  #message
     with open('phone.csv', mode='r') as phFile:
         phList = csv.reader(phFile)
         for phno in phList:
             send_url = f"http://api.ininepal.com/api/index?username=inisamarshrestha&password=nepal@123&message={msg}&destination={phno[0]}&sender=SMS"
             print(send_url)
             print(phno[0] + " " + msg)
-            #response=requests.post(send_url)
-            #print(response.text)
+            response = requests.post(send_url)
+            print(response.text)
 
     phFile.close()
 
 
-count = 0
+def sendPost():
+    msg = f"NEC New Notice:\n{title}\n{url}\n-NotifyNotice"
+    send_fb = f"https://graph.facebook.com/101231042826318/feed?message={msg}&access_token={token}"
+    response_fb = requests.post(send_fb)
+    print(response_fb.text)
+
+
+
+def getImg(url):
+  data = requests.get(url, verify=False)
+  soup = BeautifulSoup(data.text, 'html.parser')
+  postContent = soup.find('div', {'class': 'PostContent'})
+  imgUrl = postContent.find('img').get('src')
+  return imgUrl
+
 keep_alive()  #ping/call uptime robot
 
-while True:
-    data = requests.get(
-        "http://nec.edu.np/index.php?option=com_content&view=category&id=47&Itemid=74",
-        verify=False)
-    data.raise_for_status()
-    soup = BeautifulSoup(data.text, 'html.parser')
-    first = soup.find('tr', {'class': 'sectiontableentry1'})
-    title = first.find('a').text.strip()
-    link = first.find('a').get('href')
 
-    with open('notices.csv', mode='r') as file:
-        csvFile = csv.reader(file)
-        flag = 0
-        for lines in csvFile:
-            if (lines[0] == title):
-                print('NO NEW NOTICES!!')
-                flag = 1
-                break
-        if (flag == 0):
-            print('NEW NOTICE ALERT!!')
-            print("Title: ", title)
-            url = "http://nec.edu.np" + link
-            print("Link: " + url)
-            shortUrl = re.split("^https://", type_bitly.bitly.short(url))
-            print("The Shortened URL is: " + shortUrl[1])
-            wtofile()
-            sendSms()  #send sms function
+@client.event
+async def on_ready():
+    await client.wait_until_ready()
+    channel = client.get_channel(int(os.environ['channel_id']))
+    print(f"Logged in as {client.user}")
+    count = 0
+    while True:
+        data = requests.get(
+            "http://nec.edu.np/index.php?option=com_content&view=category&id=47&Itemid=74",
+            verify=False)
+        data.raise_for_status()
+        soup = BeautifulSoup(data.text, 'html.parser')
+        first = soup.find('tr', {'class': 'sectiontableentry1'})
+        title = first.find('a').text.strip()
+        link = first.find('a').get('href')
 
-        file.close()
-        count += 1
-        print("count: ", count)
-        time.sleep(60)
+        with open('notices.csv', mode='r') as file:
+            csvFile = csv.reader(file)
+            flag = 0
+            for lines in csvFile:
+                if (lines[0] == title):
+                    print('NO NEW NOTICES!!')
+                    flag = 1
+                    break
+            if (flag == 0):
+                print('NEW NOTICE ALERT!!')
+                print("Title: ", title)
+                url = "http://nec.edu.np" + link
+                print("Link: " + url)
+                shortUrl = re.split("^https://", type_bitly.bitly.short(url))
+                imgUrl = "http://nec.edu.np" + getImg(url)  #get img url
+                print("The Shortened URL is: " + shortUrl[1])
+                wtofile(title, url, shortUrl[1])
+                sendSms(title, shortUrl[1])  #send sms function
+                #sendPost()  #post on page
+                                
+                async with aiohttp.ClientSession() as session:  # creates session
+                    async with session.get(imgUrl,verify_ssl=False) as resp:  # gets image from url
+                        img = await resp.read()  # reads image from response
+                        with BytesIO(img) as file:  # converts to file-like object
+                          await channel.send(
+                    f"**_NEC_ New Notice:**\n{title}\n{url}\n-NotifyNotice",file=discord.File(file, "testimage.png"))
+                          
 
-    # print("Title: ", title)
-    # print("Link: http://nec.edu.np"+link)
-    # print("Hits: ",hits)
-    # count+=1
-    # print("count: ",count)
-    # print(dt.datetime.now())
-    #
+            file.close()
+            count = count + 1
+            print("count: ", count)
+            await asyncio.sleep(60)
+
+
+try:
+  client.run(os.environ['disToken'])
+except:
+  os.system("kill 1")
+
+# print("Title: ", title)
+# print("Link: http://nec.edu.np"+link)
+# print("Hits: ",hits)
+# count+=1
+# print("count: ",count)
+# print(dt.datetime.now())
+#
 
 #csv:
 
